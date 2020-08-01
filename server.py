fbe742720c71bf5b69505e3b9e464996747e1597
Roee <roeeavir@gmail.com>
2020-08-02 00:22:00 +0300

    Chat works now

--- server.py
+++ server.py
@@ -6,8 +6,8 @@ from threading import Thread
 def accept_connection():
     while True:
         c, ca = SERVER.accept()
-        print("Time is {}:{}\n client {} has connected".format(time.localtime().tm_hour,time.localtime().tm_min,
-                                                                ca))
+        print("Time is {}:{}\n client {} has connected".format(time.localtime().tm_hour, time.localtime().tm_min,
+                                                               ca))
         c.send(bytes("You are now connected to chat!", "utf8"))
         addresses[c] = ca
         Thread(target=handle_client, args=(c,)).start()
@@ -15,22 +15,22 @@ def accept_connection():
 
 def handle_client(c):
     name = c.recv(BUFSIZ).decode("utf8")
-    try:
-        welcome = 'Hello %s!\nIf you want to quit, type {quit} to exit.' % name
-        c.send(bytes(welcome, "utf8"))
+    # try:
+    welcome = 'Hello %s!\nIf you want to quit, type {quit} to exit.' % name
+    c.send(bytes(welcome, "utf8"))
 
-        msg = "Mezuval {} has joined the chat".format(name)
+    msg = "Mezuval {} has joined the chat".format(name)
 
-        broadcast(bytes(msg, "utf8"))
-    except OSError:
-        print("{} has disconnected from chat!".format(name))
-        clients[c] = name
+    broadcast(bytes(msg, "utf8"))
+    # except OSError:
+    #     print("{} has made an error!".format(name))
+    clients[c] = name
 
     while True:
-        try:
+        # try:
         msg = c.recv(BUFSIZ)
         if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, name+": ")
+            broadcast(msg, name + ": ")
         else:
             c.send(bytes("{quit}", "utf8"))
             c.close()
@@ -38,7 +38,8 @@ def handle_client(c):
             msg = "Mezuval {} has quit the fun".format(name)
             broadcast(bytes(msg, "utf8"))
             break
-
+        # except OSError:
+        #     print("{} has made an error!".format(name))
 
 
 def broadcast(msg, prefix=""):
