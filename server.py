54060156e8e619f9f416fad29d7502ccbfae9e10
Roee <roeeavir@gmail.com>
2020-08-01 22:11:47 +0300

    Server code is complete...?

--- server.py
+++ server.py
@@ -1,1 +1,59 @@
+import socket
+import time
+from threading import Thread
 
+
+def accept_connection():
+    while True:
+        c, ca = SERVER.accept()
+        print("{}:\n client {} has connected".format(time.localtime(), ca))
+        c.send(bytes("Please kill a me", "utf8"))
+        addresses[c] = ca
+        Thread(target=handle_client, args=(c,)).start()
+
+
+def handle_client(c):
+    name = c.recv(BUFSIZ).decode()
+
+    welcome = 'Hello %s !\nIf you want to quit, type {quit} to exit.' % name
+    c.send(bytes(welcome, "utf8"))
+
+    msg = "Mezuval {} has joined the fun".format(name)
+
+    broadcast(bytes(msg, "utf8"))
+    clients[c] = name
+
+    while True:
+        msg = c.recv(BUFSIZ)
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(bytes(msg, name + ": "))
+        else:
+            c.send(bytes("{quit}", "utf8"))
+            c.close()
+            del clients[c]
+            msg = "Mezuval {} has quit the fun".format(name)
+            broadcast(bytes(msg, "utf8"))
+            break
+
+
+def broadcast(msg, prefix=""):
+    for s in clients:
+        s.send(bytes(prefix, "utf8") + msg)
+
+
+clients = {}
+addresses = {}
+HOST = ''
+PORT = 33000
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER.bind(ADDR)
+
+if __name__ == "__main__":
+    SERVER.listen(10)
+    print("Connection is open!\nWaiting for clients...")
+    A_THREAD = Thread(target=accept_connection())
+    A_THREAD.start()
+    A_THREAD.join()
+    SERVER.close()
