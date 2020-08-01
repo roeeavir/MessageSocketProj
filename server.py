97bc8b261a83c6a9b85053c2b8358ffbd8d7fcfc
Tom Varsano <39953979+tomvarsano@users.noreply.github.com>
2020-08-01 23:38:00 +0200

    server quality changes

--- server.py
+++ server.py
@@ -6,12 +6,14 @@ from threading import Thread
 def accept_connection():
     while True:
         c, ca = SERVER.accept()
-        print("Time is {}:{}\n client {} has connected".format(time.localtime().tm_hour, time.localtime().tm_min,
-                                                               ca))
+        print("Time is {}:{}\n client {} has connected".format(
+            time.localtime().tm_hour,
+            time.localtime().tm_min,
+            ca
+        ))
         c.send(bytes("You are now connected to chat!", "utf8"))
         addresses[c] = ca
         Thread(target=handle_client, args=(c,)).start()
-
 
 def handle_client(c):
     name = c.recv(BUFSIZ).decode("utf8")
@@ -20,14 +22,11 @@ def handle_client(c):
     c.send(bytes(welcome, "utf8"))
 
     msg = "Mezuval {} has joined the chat".format(name)
-
     broadcast(bytes(msg, "utf8"))
-    # except OSError:
-    #     print("{} has made an error!".format(name))
+    
     clients[c] = name
 
     while True:
-        # try:
         msg = c.recv(BUFSIZ)
         if msg != bytes("{quit}", "utf8"):
             broadcast(msg, name + ": ")
@@ -38,14 +37,10 @@ def handle_client(c):
             msg = "Mezuval {} has quit the fun".format(name)
             broadcast(bytes(msg, "utf8"))
             break
-        # except OSError:
-        #     print("{} has made an error!".format(name))
-
 
 def broadcast(msg, prefix=""):
     for s in clients:
         s.send(bytes(prefix, "utf8") + msg)
-
 
 clients = {}
 addresses = {}
